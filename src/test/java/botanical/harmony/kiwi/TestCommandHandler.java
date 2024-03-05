3b6ed37ec9c0f59b5a7fd6e6e61852d237fab3d2
lr <lr@lr.de>
2024-03-05 17:18:38 +0100

    Introduce TestCommandHandler for testing

--- src/test/java/botanical/harmony/kiwi/TestCommandHandler.java
+++ src/test/java/botanical/harmony/kiwi/TestCommandHandler.java
@@ -1,6 +1,16 @@
 package botanical.harmony.kiwi;
+import java.util.function.Consumer;
 
-public class TestCommandHandler {
-  public TestCommandHandler(Object o) {
+public class TestCommandHandler implements CommandHandler<TestCommand> {
+  private final Consumer<TestCommand> testCommandConsumer;
+
+  public TestCommandHandler(Consumer<TestCommand> testCommandConsumer) {
+    this.testCommandConsumer = testCommandConsumer;
+  }
+
+  @Override
+  public CommandResponse handle(TestCommand testCommand) {
+    testCommandConsumer.accept(testCommand);
+    return CommandResponse.success();
   }
 }
