5754578b089211e462841e8aa406e91e295ebe1f
lr <lr@lr.de>
2024-03-05 17:18:27 +0100

    Introduce TestCommand for testing

--- src/test/java/botanical/harmony/kiwi/TestCommand.java
+++ src/test/java/botanical/harmony/kiwi/TestCommand.java
@@ -1,2 +1,14 @@
-package botanical.harmony.kiwi;public class TestCommand {
+package botanical.harmony.kiwi;
+
+public class TestCommand implements Command {
+  private final int value;
+
+  public TestCommand(int value) {
+
+    this.value = value;
+  }
+
+  public int getValue() {
+    return value;
+  }
 }
