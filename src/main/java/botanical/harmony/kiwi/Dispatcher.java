ba0112f2f028881b03a690f55c747b564f29ea79
lr <lr@lr.de>
2024-03-06 09:54:59 +0100

    Introduce Dispatcher

---
+++ src/main/java/botanical/harmony/kiwi/Dispatcher.java
@@ -0,0 +1,11 @@
+package botanical.harmony.kiwi;
+
+public class Dispatcher {
+  public static Dispatcher create() {
+    return null;
+  }
+
+  public CommandResponse dispatch(Command command) {
+    return null;
+  }
+}
