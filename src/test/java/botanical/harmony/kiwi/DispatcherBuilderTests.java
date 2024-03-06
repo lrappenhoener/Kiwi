c31614f68439b8d8884446ff51a134e0b1af26e0
lr <lr@lr.de>
2024-03-06 09:56:00 +0100

    Introduce DispatcherBuilerTests

--- src/test/java/botanical/harmony/kiwi/DispatcherBuilderTests.java
+++ src/test/java/botanical/harmony/kiwi/DispatcherBuilderTests.java
@@ -1,2 +1,24 @@
-package botanical.harmony.kiwi;public class DispatcherBuilderTests {
+package botanical.harmony.kiwi;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DispatcherBuilderTests {
+  @Test
+  void correctly_knows_if_handler_is_registered(){
+    DispatcherBuilder builder = DispatcherBuilder.create();
+    TestCommandHandler handler = new TestCommandHandler((c) -> {});
+    builder.register(handler);
+
+    assertTrue(builder.hasCommandHandlerFor(TestCommand.class));
+  }
+
+  @Test
+  void correctly_knows_if_handler_is_not_registered() {
+    DispatcherBuilder builder = DispatcherBuilder.create();
+
+    assertFalse(builder.hasCommandHandlerFor(TestCommand.class));
+  }
 }
