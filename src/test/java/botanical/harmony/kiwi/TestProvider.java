6b1b8759965dec55e99e75aae15e3c05f9b571eb
lr <lr@lr.de>
2024-03-07 14:56:22 +0100

    Introduce TestProvider

--- src/test/java/botanical/harmony/kiwi/TestProvider.java
+++ src/test/java/botanical/harmony/kiwi/TestProvider.java
@@ -1,2 +1,12 @@
-package botanical.harmony.kiwi;public class TestProvider {
+package botanical.harmony.kiwi;
+
+public class TestProvider implements HandlerProvider {
+  public static TestProvider create() {
+    return new TestProvider();
+  }
+
+  @Override
+  public boolean canProvide(Class<?> clazz) {
+    return true;
+  }
 }
