54a37f60a7db83eb8d3ae921742e4bc684a15fac
lr <lr@lr.de>
2024-03-07 20:30:25 +0100

    Update dispatch of Dispatcher

--- src/main/java/botanical/harmony/kiwi/Dispatcher.java
+++ src/main/java/botanical/harmony/kiwi/Dispatcher.java
@@ -1,14 +1,42 @@
 package botanical.harmony.kiwi;
 
+import java.util.Map;
+import java.util.Optional;
+
 public class Dispatcher {
-  private Dispatcher() {
+  private final Map<Class<? extends Command>, CommandHandler<? extends Command>> commandHandlers;
+  private final Map<Class<? extends Command>, Class<? extends CommandHandler<?>>> commandHandlerClasses;
+  private final Optional<HandlerProvider> optionalHandlerProvider;
+
+  private Dispatcher(
+          Map<Class<? extends Command>, CommandHandler<? extends Command>> commandHandlers,
+          Map<Class<? extends Command>, Class<? extends CommandHandler<?>>> commandHandlerClasses,
+          Optional<HandlerProvider> optionalHandlerProvider) {
+    this.commandHandlers = commandHandlers;
+    this.commandHandlerClasses = commandHandlerClasses;
+    this.optionalHandlerProvider = optionalHandlerProvider;
   }
 
-  public static Dispatcher create() {
-    return new Dispatcher();
+  public static Dispatcher create(
+          Map<Class<? extends Command>, CommandHandler<? extends Command>> commandHandlers,
+          Map<Class<? extends Command>, Class<? extends CommandHandler<?>>> commandHandlerClasses,
+          Optional<HandlerProvider> optionalHandlerProvider) {
+    return new Dispatcher(commandHandlers, commandHandlerClasses, optionalHandlerProvider);
   }
 
-  public CommandResponse dispatch(Command command) {
+  public CommandResponse dispatch(
+          Command command) {
+    Class<? extends Command> commandClass = command.getClass();
+    if (commandHandlers.containsKey(commandClass)) {
+      CommandHandler commandHandler = commandHandlers.get(commandClass);
+      return commandHandler.handle(command);
+    }
+    if (commandHandlerClasses.containsKey(commandClass)) {
+      Class<? extends CommandHandler<?>> commandHandlerClass = commandHandlerClasses.get(
+              commandClass);
+      CommandHandler commandHandler = optionalHandlerProvider.get().provide(commandHandlerClass);
+      return commandHandler.handle(command);
+    }
     return CommandResponse.failed("no handler registered");
   }
 }
