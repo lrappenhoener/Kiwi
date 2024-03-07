a160591f040cb5fee2f0f2453bd0d8b835492d02
lr <lr@lr.de>
2024-03-07 22:22:30 +0100

    Update HandlerProvider

--- src/main/java/botanical/harmony/kiwi/HandlerProvider.java
+++ src/main/java/botanical/harmony/kiwi/HandlerProvider.java
@@ -2,6 +2,5 @@ package botanical.harmony.kiwi;
 
 public interface HandlerProvider {
   boolean canProvide(Class<?> clazz);
-
-  CommandHandler provide(Class<? extends CommandHandler<?>> commandHandlerClass);
+  <T> T provide(Class<T> clazz);
 }
