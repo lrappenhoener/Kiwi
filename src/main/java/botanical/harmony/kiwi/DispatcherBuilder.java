ca6de71c58dead877b9ee6a55412d29e630fcc2a
lr <lr@lr.de>
2024-03-07 15:44:59 +0100

    Format code

--- src/main/java/botanical/harmony/kiwi/DispatcherBuilder.java
+++ src/main/java/botanical/harmony/kiwi/DispatcherBuilder.java
@@ -6,10 +6,8 @@ import java.util.Map;
 import java.util.Optional;
 
 public class DispatcherBuilder {
-  private final Map<Class<? extends Command>,
-          CommandHandler<? extends Command>> commandHandlers = new HashMap<>();
-  private final Map<Class<? extends Command>,
-          Class<? extends CommandHandler<?>>> commandHandlerClasses = new HashMap<>();
+  private final Map<Class<? extends Command>, CommandHandler<? extends Command>> commandHandlers = new HashMap<>();
+  private final Map<Class<? extends Command>, Class<? extends CommandHandler<?>>> commandHandlerClasses = new HashMap<>();
   private final Optional<HandlerProvider> optionalHandlerProvider;
 
   private DispatcherBuilder(Optional<HandlerProvider> optionalHandlerProvider) {
@@ -31,6 +29,23 @@ public class DispatcherBuilder {
     return this;
   }
 
+  public Dispatcher build() {
+    return Dispatcher.create();
+  }
+
+  public boolean hasCommandHandlerFor(Class<? extends Command> commandClass) {
+    return commandHandlers.containsKey(commandClass) || commandHandlerClasses.containsKey(
+            commandClass);
+  }
+
+  public <TCommand extends Command> DispatcherBuilder register(Class<? extends CommandHandler<TCommand>> handlerClass) {
+    if (optionalHandlerProvider.isEmpty()) throw new MissingProviderException(
+            "You need to create the builder with an provider to create handler instances if you want to register by class");
+    Class<TCommand> commandClass = getCommandClass(handlerClass);
+    commandHandlerClasses.put(commandClass, handlerClass);
+    return this;
+  }
+
   private <TCommand extends Command> Class<TCommand> getCommandClass(
           CommandHandler<TCommand> handler) {
     Class<? extends CommandHandler> handlerClass = handler.getClass();
@@ -40,22 +55,5 @@ public class DispatcherBuilder {
   private <TCommand extends Command> Class<TCommand> getCommandClass(Class<? extends CommandHandler> handlerClass) {
     ParameterizedType parameterizedType = (ParameterizedType) handlerClass.getGenericInterfaces()[0];
     return (Class<TCommand>) parameterizedType.getActualTypeArguments()[0];
-  }
-
-  public Dispatcher build() {
-    return Dispatcher.create();
-  }
-
-  public boolean hasCommandHandlerFor(Class<? extends Command> commandClass) {
-    return commandHandlers.containsKey(commandClass) || commandHandlerClasses.containsKey(commandClass);
-  }
-
-  public <TCommand extends Command> DispatcherBuilder register(Class<? extends CommandHandler<TCommand>> handlerClass) {
-    if (optionalHandlerProvider.isEmpty())
-      throw new MissingProviderException(
-              "You need to create the builder with an provider to create handler instances if you want to register by class");
-    Class<TCommand> commandClass = getCommandClass(handlerClass);
-    commandHandlerClasses.put(commandClass, handlerClass);
-    return this;
   }
 }
