db5ddb79c035a51ae5e90a916b0f79889de0c434
lr <lr@lr.de>
2024-03-07 14:59:41 +0100

    Add DispatcherBuilder tests

--- src/test/java/botanical/harmony/kiwi/DispatcherBuilderTests.java
+++ src/test/java/botanical/harmony/kiwi/DispatcherBuilderTests.java
@@ -2,14 +2,20 @@ package botanical.harmony.kiwi;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import botanical.harmony.strawberry.Container;
+import botanical.harmony.strawberry.ContainerBuilder;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DispatcherBuilderTests {
   @Test
-  void correctly_knows_if_handler_is_registered(){
+  void correctly_knows_if_handler_is_registered() {
+
+    ContainerBuilder container;
+
     DispatcherBuilder builder = DispatcherBuilder.create();
-    TestCommandHandler handler = new TestCommandHandler((c) -> {});
+    TestCommandHandler handler = new TestCommandHandler((c) -> {
+    });
     builder.register(handler);
 
     assertTrue(builder.hasCommandHandlerFor(TestCommand.class));
@@ -20,5 +26,27 @@ public class DispatcherBuilderTests {
     DispatcherBuilder builder = DispatcherBuilder.create();
 
     assertFalse(builder.hasCommandHandlerFor(TestCommand.class));
+  }
+
+  @Test
+  void throws_missing_provider_exception_when_trying_register_handler_class_without_provider() {
+    DispatcherBuilder builder = DispatcherBuilder.create();
+
+    assertThrows(MissingProviderException.class, () -> builder.register(
+            TestCommandHandler.class));
+  }
+
+  @Test
+  void correctly_knows_if_handler_class_registered() {
+    TestProvider testProvider = TestProvider.create();
+    DispatcherBuilder builder = DispatcherBuilder.create(testProvider);
+    builder.register(TestCommandHandler.class);
+
+    assertTrue(builder.hasCommandHandlerFor(TestCommand.class));
+  }
+
+  @Test
+  void correctly_knows_if_handler_is_not_resolvable_by_the_provider() {
+
   }
 }
