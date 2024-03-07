6009f131c7fa1eea3b91c0a27850285fea25b01b
lr <lr@lr.de>
2024-03-07 22:22:39 +0100

    Update TestProvider

--- src/test/java/botanical/harmony/kiwi/TestProvider.java
+++ src/test/java/botanical/harmony/kiwi/TestProvider.java
@@ -1,6 +1,12 @@
 package botanical.harmony.kiwi;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
 public class TestProvider implements HandlerProvider {
+  private final Map<Class<?>, Supplier<?>> handlerFactories = new HashMap<>();
+
   public static TestProvider create() {
     return new TestProvider();
   }
@@ -11,7 +17,14 @@ public class TestProvider implements HandlerProvider {
   }
 
   @Override
-  public CommandHandler provide(Class<? extends CommandHandler<?>> commandHandlerClass) {
-    return null;
+  public <T> T provide(Class<T> clazz) {
+    return (T)handlerFactories.get(clazz).get();
+  }
+
+  public TestProvider register(
+          Class<?> clazz,
+          Supplier<CommandHandler<?>> commandHandlerFactory) {
+    handlerFactories.put(clazz, commandHandlerFactory);
+    return this;
   }
 }
