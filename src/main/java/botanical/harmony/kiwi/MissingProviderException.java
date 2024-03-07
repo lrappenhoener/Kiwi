718fc7847c0e7aa242502a9fb8f029c67f8164fa
lr <lr@lr.de>
2024-03-07 14:54:44 +0100

    Introduce MissingProviderException

--- src/main/java/botanical/harmony/kiwi/MissingProviderException.java
+++ src/main/java/botanical/harmony/kiwi/MissingProviderException.java
@@ -1,4 +1,15 @@
 package botanical.harmony.kiwi;
 
 public class MissingProviderException extends RuntimeException {
+  private final String message;
+
+  public MissingProviderException(String message){
+
+    this.message = message;
+  }
+
+  @Override
+  public String getMessage() {
+    return message;
+  }
 }
