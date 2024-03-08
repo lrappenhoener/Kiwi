56fe8fdb6f468d270c7a1917932288d0db84021f
lr <lr@lr.de>
2024-03-08 08:49:10 +0100

    Add send_query_without_registered_handler_returns_not_successful_respon│

--- src/test/java/botanical/harmony/kiwi/QueryDispatchTests.java
+++ src/test/java/botanical/harmony/kiwi/QueryDispatchTests.java
@@ -1,5 +1,20 @@
 package botanical.harmony.kiwi;
 
-public class QueryDispatchTests {
+import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class QueryDispatchTests {
+  @Test
+  void send_query_without_registered_handler_returns_not_successful_response() {
+    DispatcherBuilder builder = DispatcherBuilder.create();
+    Dispatcher dispatcher = builder.build();
+    Query<List<String>> query = new TestQuery(new String[]{"foo", "bar"});
+
+    QueryResponse<List<String>> response = dispatcher.send(query);
+
+    assertFalse(response.isSuccessful());
+  }
 }
