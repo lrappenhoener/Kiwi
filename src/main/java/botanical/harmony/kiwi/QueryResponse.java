291c979021172973aeb1405385407514e96ad272
lr <lr@lr.de>
2024-03-09 09:30:04 +0100

    Update QueryResponse

--- src/main/java/botanical/harmony/kiwi/QueryResponse.java
+++ src/main/java/botanical/harmony/kiwi/QueryResponse.java
@@ -1,17 +1,30 @@
 package botanical.harmony.kiwi;
 
+import java.util.Optional;
+
 public class QueryResponse<T> {
   private final boolean successful;
   private final String message;
+  private final Optional<T> result;
 
   public QueryResponse(boolean successful, String message) {
-
     this.successful = successful;
     this.message = message;
+    result = Optional.empty();
+  }
+
+  public QueryResponse(boolean successful, String message, T result) {
+    this.successful = successful;
+    this.message = message;
+    this.result = Optional.of(result);
   }
 
   public static <T> QueryResponse<T> failed(String message) {
     return new QueryResponse<>(false, message);
+  }
+
+  public static <T> QueryResponse<T> success(T result, String message) {
+    return new QueryResponse<>(true, message, result);
   }
 
   public boolean isSuccessful() {
@@ -20,5 +33,9 @@ public class QueryResponse<T> {
 
   public String getMessage() {
     return message;
+  }
+
+  public Optional<T> getResult() {
+    return result;
   }
 }
