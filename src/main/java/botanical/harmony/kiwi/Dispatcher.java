3a05fec5f5fd6cc4d1a84830856fe091699b0a13
lr <lr@lr.de>
2024-03-09 09:31:37 +0100

    Update Dispatcher

--- src/main/java/botanical/harmony/kiwi/Dispatcher.java
+++ src/main/java/botanical/harmony/kiwi/Dispatcher.java
@@ -1,28 +1,41 @@
 package botanical.harmony.kiwi;
 
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 public class Dispatcher {
   private final Map<Class<? extends Command>, CommandHandler<? extends Command>> commandHandlers;
   private final Map<Class<? extends Command>, Class<? extends CommandHandler<?>>> commandHandlerClasses;
+  private final Map<Class<? extends Query>, QueryHandler> queryHandlers;
+  private final Map<Class<? extends Query>, Class<? extends QueryHandler<?, ?>>> queryHandlerClasses;
   private final Optional<HandlerProvider> optionalHandlerProvider;
 
   private Dispatcher(
           Map<Class<? extends Command>, CommandHandler<? extends Command>> commandHandlers,
           Map<Class<? extends Command>, Class<? extends CommandHandler<?>>> commandHandlerClasses,
+          Map<Class<? extends Query>, QueryHandler> queryHandlers,
+          Map<Class<? extends Query>, Class<? extends QueryHandler<?, ?>>> queryHandlerClasses,
           Optional<HandlerProvider> optionalHandlerProvider) {
     this.commandHandlers = commandHandlers;
     this.commandHandlerClasses = commandHandlerClasses;
+    this.queryHandlers = queryHandlers;
+    this.queryHandlerClasses = queryHandlerClasses;
     this.optionalHandlerProvider = optionalHandlerProvider;
   }
 
   public static Dispatcher create(
           Map<Class<? extends Command>, CommandHandler<? extends Command>> commandHandlers,
           Map<Class<? extends Command>, Class<? extends CommandHandler<?>>> commandHandlerClasses,
+          Map<Class<? extends Query>, QueryHandler> queryHandlers,
+          Map<Class<? extends Query>, Class<? extends QueryHandler<?, ?>>> queryHandlerClasses,
           Optional<HandlerProvider> optionalHandlerProvider) {
-    return new Dispatcher(commandHandlers, commandHandlerClasses, optionalHandlerProvider);
+    return new Dispatcher(
+            commandHandlers,
+            commandHandlerClasses,
+            queryHandlers,
+            queryHandlerClasses,
+            optionalHandlerProvider
+    );
   }
 
   public CommandResponse send(
@@ -42,6 +55,11 @@ public class Dispatcher {
   }
 
   public <T> QueryResponse<T> send(Query<T> query) {
+    Class<? extends Query> queryClass = query.getClass();
+    if (queryHandlers.containsKey(queryClass)) {
+      QueryHandler queryHandler = queryHandlers.get(queryClass);
+      return queryHandler.handle(query);
+    }
     return QueryResponse.failed("no handler registered");
   }
 }
