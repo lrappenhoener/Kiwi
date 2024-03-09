5ce6fa23c781eb328c7fdf1007191f70fa9c70a2
lr <lr@lr.de>
2024-03-09 09:32:51 +0100

    Introduce TestQuery

--- src/test/java/botanical/harmony/kiwi/TestQuery.java
+++ src/test/java/botanical/harmony/kiwi/TestQuery.java
@@ -3,6 +3,13 @@ package botanical.harmony.kiwi;
 import java.util.List;
 
 public class TestQuery implements Query<List<String>> {
-  public TestQuery(String[] strings) {
+  private final List<String> strings;
+
+  public TestQuery(List<String> strings) {
+    this.strings = strings;
+  }
+
+  public List<String> getStrings() {
+    return strings;
   }
 }
