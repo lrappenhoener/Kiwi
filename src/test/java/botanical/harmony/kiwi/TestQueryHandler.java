e0a946c49ba48312c0ab3de0f0f36c0ef6e8b604
lr <lr@lr.de>
2024-03-09 09:32:35 +0100

    Introduce TestQueryHandler

--- src/test/java/botanical/harmony/kiwi/TestQueryHandler.java
+++ src/test/java/botanical/harmony/kiwi/TestQueryHandler.java
@@ -1,2 +1,10 @@
-package botanical.harmony.kiwi;public class TestQueryHandler {
+package botanical.harmony.kiwi;
+
+import java.util.List;
+
+public class TestQueryHandler implements QueryHandler<TestQuery, List<String>>{
+  @Override
+  public QueryResponse<List<String>> handle(TestQuery testQuery) {
+    return QueryResponse.success(testQuery.getStrings(), "Successful");
+  }
 }
